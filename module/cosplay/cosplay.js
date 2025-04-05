ac42cfe40f820cd5d6f73ac8d299b4f1fe6ae37d
tkhanh-dev <khanhshado9@gmail.com>
2025-04-05 18:28:44 +0700

    Update cosplay.js

--- module/cosplay/cosplay.js
+++ module/cosplay/cosplay.js
@@ -3,7 +3,7 @@ exports.index = async(req, res, next) => {
   var data = require('fs-extra').readFileSync(__dirname + '/cosplay.txt', 'utf-8').split(/\r?\n/);
   link = data[Math.floor(Math.random() * data.length)].trim();
   link1 = data[Math.floor(Math.random() * data.length)].trim();
-  res.json({"Authors":"Đức tài cuti vcl",
+  res.json({"author":"TKDEV",
 "url":`${link}`,
 "data":`${link1}`});
 };
